13649efcb560a233e0886a235c89c01093d14d68
shangsong <shangsong@buaa.edu.cn>
2019-01-02 14:46:49 +0800

    update

--- MerkelTree/MerkelTree.cpp
+++ MerkelTree/MerkelTree.cpp
@@ -8,36 +8,47 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	string check_str = "";
+	string check_str = "B";
 
 	cout << "Enter Merkle leafes: " << endl;
 	//叶子节点为string类
 	vector<string> v;
 
 	//输入叶子节点
-	while (1)
-	{
-		string str;
-		cin >> str;
-		if (str != ";")
-		{
-			//存入v
-			v.push_back(str);
-		}
-		else
-		{
-			break;
-		}
-	}
+	//while (1)
+	//{
+	//	string str;
+	//	cin >> str;
+	//	if (str != ";")
+	//	{
+	//		//存入v
+	//		v.push_back(str);
+	//	}
+	//	else
+	//	{
+	//		break;
+	//	}
+	//}
+	v.push_back("A");
+	v.push_back("B");
+	v.push_back("C");
+	v.push_back("D");
+	v.push_back("E");
+	v.push_back("F");
+	v.push_back("G");
+	v.push_back("H");
+	v.push_back("I");
+	v.push_back("J");
 
 	cout << "Enter leaf to verify: " << endl;
 	//输入想验证的叶子节点
-	cin >> check_str;
+	//cin >> check_str;
 	check_str = picosha2::hash256_hex_string(check_str);
 
 	tree ntree;
 	ntree.buildBaseLeafes(v);
 	ntree.buildTree();
+	ntree.merklerootexclusive(v,"");
 
 	cout << "Check if: " << check_str << " is contained in tree " << endl;
 
